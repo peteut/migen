3c147fb492b9125cce9666bbd059b0a45a609246
Alain Péteut <alain.peteut@yahoo.com>
2015-04-20 09:12:43 +0200

    add I/O standard definitions to mibuild/altera

--- mibuild/altera/__init__.py
+++ mibuild/altera/__init__.py
@@ -1,2 +1,3 @@
-from mibuild.altera.platform import AlteraPlatform
-from mibuild.altera.programmer import USBBlaster
+from mibuild.altera.platform import AlteraPlatform  # noqa
+from mibuild.altera.programmer import USBBlaster  # noqa
+from mibuild.altera._iostd import IOSTD  # noqa
