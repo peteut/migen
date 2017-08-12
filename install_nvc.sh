7de9a89ef081683c5ce4097a57b3e53b6a2335d6
Alain Péteut <alain.peteut@yahoo.com>
2017-08-12 13:56:44 +0200

    update nvc to 1.2.1 used by CI

--- install_nvc.sh
+++ install_nvc.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 set -x
-function escape_slashes {
-    sed 's/\//\\\//g'
-}
+release="1.2.1"
 if [ -z $(which nvc) ]
 then
-path="https://github.com/nickg/nvc/releases/download/r1.1.0/nvc-1.1.0.tar.gz"
+path="https://github.com/nickg/nvc/releases/download/r${release}/nvc-${release}.tar.gz"
 wget -q -N ${path}
-tar -xzvf nvc-1.1.0.tar.gz
-pushd nvc-1.1
-fetch_ieee="./tools/fetch-ieee.sh"
-gna_path=$(echo "http://svn.gna.org/svn/ghdl/trunk/libraries/vital2000" | escape_slashes)
-github_path=$(echo "https://raw.githubusercontent.com/tgingold/ghdl/master/libraries/vital2000" | escape_slashes)
-echo "gna_path: ${gna_path}"
-echo "github_path: ${github_path}"
-sed -i 's/'"${gna_path}"'/'"${github_path}"'/' ${fetch_ieee}
-sed -i 's/03878edc834050f67ec5d3e0b49bef883065dc429700a174e93850274c63e458/507163c4530ffe894929caf604437c9a24dd8f4b347f0c01949d230fdfcd2825/' ${fetch_ieee}
-
+tar -xzvf nvc-${release}.tar.gz
+pushd nvc-${release}
 ./tools/fetch-ieee.sh && ./configure --prefix=$HOME
 make && make install
 mkdir -p $HOME/.nvc/lib
