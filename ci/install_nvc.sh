369c6bd4cda6bfc62185184832d9d62959f1a6d7
Alain Péteut <alain.peteut@yahoo.com>
2018-06-10 00:04:08 +0200

    CI: update nvc release and apply patch

--- ci/install_nvc.sh
+++ ci/install_nvc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
-release="1.2.1"
+release="1.3.1"
 if [ -z $(which nvc) ]
 then
 path="https://github.com/nickg/nvc/releases/download/r${release}/nvc-${release}.tar.gz"
-wget -q -N ${path}
-tar -xzvf nvc-${release}.tar.gz
+curl -sL  ${path} | tar -xzf -
 pushd nvc-${release}
+patch -p1 < ../ci/ieee-download-webpage-has-changed-375.patch
 ./tools/fetch-ieee.sh && ./configure --prefix=${HOME} --with-llvm=${LLVM_CONFIG}
 make && make install
 mkdir -p ${HOME}/.nvc/lib
