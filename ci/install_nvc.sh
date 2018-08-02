e86e133c2598e843cd94a886bb330d7971e58434
Alain Péteut <alain.peteut@yahoo.com>
2018-08-03 00:58:26 +0200

    CI: bump nvc release to 1.4.0

--- ci/install_nvc.sh
+++ ci/install_nvc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -x
-release="1.3.1"
+release="1.4.0"
 if [ -z $(which nvc) ]
 then
 path="https://github.com/nickg/nvc/releases/download/r${release}/nvc-${release}.tar.gz"
 curl -sL  ${path} | tar -xzf -
 pushd nvc-${release}
-patch -p1 < ../ci/ieee-download-webpage-has-changed-375.patch
 ./tools/fetch-ieee.sh && ./configure --prefix=${HOME} --with-llvm=${LLVM_CONFIG}
 make && make install
 mkdir -p ${HOME}/.nvc/lib
